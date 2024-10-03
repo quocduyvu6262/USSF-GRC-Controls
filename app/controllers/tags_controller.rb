af8bdd02f23faaa74773dcd60c0658e64a1ac898
quocduyvu6262 <quocduyvu6262@gmail.com>
2024-10-03 15:07:51 -0500

    added image show page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,9 +2,11 @@
 class TagsController < ApplicationController
     def show
       @tag = params[:id]  # Assuming the tag is passed as the ID
+      @user = current_user
     end
 
     def new
+      @user = current_user
     end
 
 end
