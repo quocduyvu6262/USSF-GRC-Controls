604b8244269dc6136796cdbaa6957f9cbead94be
Vasudha Devarakonda <vasudhadevarakonda@tamu.edu>
2024-10-04 09:09:48 -0500

    add route for trivy scan

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,4 +8,29 @@ class TagsController < ApplicationController
     def new
       @user = current_user
     end
+    #scan image based on parameter 
+
+
+    def create
+      @movie = Movie.create!(movie_params)
+      flash[:notice] = "#{@movie.title} was successfully created."
+      redirect_to movies_path
+    end
+
+    def create
+      @image = Image.new(image_params)
+      if @image.save
+        redirect_to @image, notice: 'Image was successfully created.'
+      else
+        render :new
+      end
+    end
+
+    
+    def scan_image
+      @result = `trivy image python:3.4-alpine`
+      if $?.exitstatus != 0
+        @error_message = "Error running Trivy"
+      end
+    end
 end
