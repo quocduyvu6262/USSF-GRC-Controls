1bc24eccd13d7bc41b7af936297d52e22d6a3718
Vasudha Devarakonda <vasudhadevarakonda@tamu.edu>
2024-10-04 20:24:20 -0500

    fix rubocop

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,36 +17,36 @@ RSpec.describe "Users", type: :request do
   let!(:rto1) { RunTimeObject.create!(name: "Object 1", description: "Description of Object 1", user_id: user.id) }
   let!(:rto2) { RunTimeObject.create!(name: "Object 2", description: "Description of Object 2", user_id: user.id) }
 
- 
+
   let!(:image1) { Image.create!(tag: "Image 1", report: "Report for image 1", run_time_object_id: rto1.id) }
   let!(:image2) { Image.create!(tag: "Image 2", report: "Report for image 2", run_time_object_id: rto2.id) }  # Changed r2 to rto2
 
 
   before do
-    get '/auth/google_oauth2/callback'  
+    get '/auth/google_oauth2/callback'
   end
 
   describe "GET /users/:id" do
     it "assigns @rto_list with the runtime objects for the user" do
-      get user_path(user.id) 
-      expect(response).to be_successful  
-      rto_list = RunTimeObject.where(user_id: user.id)  
-      actual_rto_list = assigns(:rto_list)  
-      expect(actual_rto_list).to match_array(rto_list)  
+      get user_path(user.id)
+      expect(response).to be_successful
+      rto_list = RunTimeObject.where(user_id: user.id)
+      actual_rto_list = assigns(:rto_list)
+      expect(actual_rto_list).to match_array(rto_list)
     end
 
     it "assigns @image_list with images from runtime objects for the user" do
-      get user_path(user.id)  
-      rto_list = RunTimeObject.where(user_id: user.id)  
-      image_list = Image.where(run_time_object_id: rto_list.pluck(:id))  
+      get user_path(user.id)
+      rto_list = RunTimeObject.where(user_id: user.id)
+      image_list = Image.where(run_time_object_id: rto_list.pluck(:id))
 
-      actual_image_list = assigns(:image_list) 
-      expect(actual_image_list).to match_array(image_list) 
+      actual_image_list = assigns(:image_list)
+      expect(actual_image_list).to match_array(image_list)
     end
 
     it "returns a successful response" do
       get user_path(user.id)
-      expect(response).to have_http_status(:success)  
+      expect(response).to have_http_status(:success)
     end
   end
 end
